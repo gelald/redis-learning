152fc6273dcd435837af21153585e6a7401736d0
gelald <37375083+gelald@users.noreply.github.com>
2025-07-10 17:20:55 +0800

    chore(double-level-cache) double level cache project init

---
+++ double-level-cache/src/main/java/com/github/gelald/cache/DoubleCacheApplication.java
@@ -0,0 +1,11 @@
+package com.github.gelald.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleCacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DoubleCacheApplication.class, args);
+    }
+}
